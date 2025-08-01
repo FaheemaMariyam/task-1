b4bfbd8c0bbe5e370e9168df90af52c1da76df1a
FaheemaMariyam <faheemamariyam2102004@gmail.com>
2025-08-01 14:29:09 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import Fetching from './components/task2'; 
+function App(){
+  return(
+  <Fetching/>);
+}
+export default App ;
---
+++ test.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+const MyComponent=()=>{
+    return <div> <h1>this is from my component</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod velit harum unde perferendis consequatur? Odio sit dolore explicabo, itaque eos aliquid illum atque fuga porro, repellat, neque aut perferendis voluptas?</p>
+    </div>
+      
+};
+export default MyComponent;
